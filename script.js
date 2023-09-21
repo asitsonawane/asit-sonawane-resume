5a57902689180f6c5fe784871b5fb022fb34564b
asitsonawane <sonawaneasit@gmail.com>
2023-09-22 04:13:54 +0530

    Added features

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+for (var i = 0; i < drops.length; i++) {
+  var text = letters[Math.floor(Math.random() * letters.length)];
+  ctx.fillStyle = '#0f0';
+  ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+  drops[i]++;
+  if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
+    drops[i] = 0;
+  }
+}
+}
+
+// Apply overflow: hidden to the body
+document.body.style.overflow = 'hidden';
+
+// Schedule canvas removal after 3 seconds
+setTimeout(function () {
+  document.body.style.overflow = 'auto'; // Set overflow back to auto to restore scrolling
+  document.body.removeChild(canvas);
+}, 10000);
+
+// Loop the animation
+setInterval(draw, 33);
+
+
+
+
 function updateTime() {
   const currentTimeElement = document.getElementById('currentTime');
   const now = new Date();
